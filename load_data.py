ce98468fb491a6718c2975ca9f3c088953891d95
Matthew Zhou <Mattz0516@outlook.com>
2024-05-01 08:07:09 -0400

    Initial commit

---
+++ load_data.py
@@ -0,0 +1,158 @@
+# import system libs
+import os
+from PIL import Image
+
+# import data handling tools
+import numpy as np
+import pandas as pd
+import seaborn as sns
+sns.set_style('darkgrid')
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+
+# import Deep learning Libraries
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam, Adamax
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Activation, Dropout, BatchNormalization
+from tensorflow.keras import regularizers
+
+# Ignore Warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+print ('modules loaded')
+
+# unzip the data
+if not os.path.exists('lung_colon_image_set'):
+    !unzip archive.zip
+    
+# Generate data paths with labels
+data_dir = 'lung_colon_image_set'
+filepaths = []
+labels = []
+
+folds = os.listdir(data_dir)
+for fold in folds:
+    foldpath = os.path.join(data_dir, fold)
+    flist = os.listdir(foldpath)
+
+    for f in flist:
+        f_path = os.path.join(foldpath, f)
+        filelist = os.listdir(f_path)
+
+        for file in filelist:
+            fpath = os.path.join(f_path, file)
+            filepaths.append(fpath)
+
+            if f == 'colon_aca':
+                labels.append('Colon Adenocarcinoma')
+
+            elif f == 'colon_n':
+                labels.append('Colon Benign Tissue')
+
+            elif f == 'lung_aca':
+                labels.append('Lung Adenocarcinoma')
+
+            elif f == 'lung_n':
+                labels.append('Lung Benign Tissue')
+
+            elif f == 'lung_scc':
+                labels.append('Lung Squamous Cell Carcinoma')
+
+# Concatenate data paths with labels into one dataframe
+Fseries = pd.Series(filepaths, name= 'filepaths')
+Lseries = pd.Series(labels, name='labels')
+df = pd.concat([Fseries, Lseries], axis= 1)
+
+strat = df['labels']
+train_df, dummy_df = train_test_split(df,  train_size= 0.8, shuffle= True, random_state= 123, stratify= strat)
+
+# valid and test dataframe
+strat = dummy_df['labels']
+valid_df, test_df = train_test_split(dummy_df,  train_size= 0.5, shuffle= True, random_state= 123, stratify= strat)
+
+def view_image(n, df=df):
+    img = Image.open(df['filepaths'].iloc[n])
+    plt.imshow(img)
+    plt.title(df['labels'].iloc[n])
+    plt.axis('off')
+    plt.show()
+    
+def load_data(df, batch_size):
+    # unzip the data
+    if not os.path.exists('lung_colon_image_set'):
+        # unzip archive.zip
+        import zipfile
+        with zipfile.ZipFile('archive.zip', 'r') as zip_ref:
+            zip_ref.extractall('.')
+        
+        
+    # Generate data paths with labels
+    data_dir = 'lung_colon_image_set'
+    filepaths = []
+    labels = []
+
+    folds = os.listdir(data_dir)
+    for fold in folds:
+        foldpath = os.path.join(data_dir, fold)
+        flist = os.listdir(foldpath)
+
+        for f in flist:
+            f_path = os.path.join(foldpath, f)
+            filelist = os.listdir(f_path)
+
+            for file in filelist:
+                fpath = os.path.join(f_path, file)
+                filepaths.append(fpath)
+
+                if f == 'colon_aca':
+                    labels.append('Colon Adenocarcinoma')
+
+                elif f == 'colon_n':
+                    labels.append('Colon Benign Tissue')
+
+                elif f == 'lung_aca':
+                    labels.append('Lung Adenocarcinoma')
+
+                elif f == 'lung_n':
+                    labels.append('Lung Benign Tissue')
+
+                elif f == 'lung_scc':
+                    labels.append('Lung Squamous Cell Carcinoma')
+
+    # Concatenate data paths with labels into one dataframe
+    Fseries = pd.Series(filepaths, name= 'filepaths')
+    Lseries = pd.Series(labels, name='labels')
+    df = pd.concat([Fseries, Lseries], axis= 1)
+    
+    strat = df['labels']
+    train_df, dummy_df = train_test_split(df,  train_size= 0.8, shuffle= True, random_state= 123, stratify= strat)
+
+    # valid and test dataframe
+    strat = dummy_df['labels']
+    valid_df, test_df = train_test_split(dummy_df,  train_size= 0.5, shuffle= True, random_state= 123, stratify= strat)
+    
+    # crobed image size
+    batch_size = 32
+    img_size = (224, 224)
+    channels = 3
+    # img_shape = (img_size[0], img_size[1], channels)
+
+    # augment the training data
+    tr_gen = ImageDataGenerator(rescale= 1./255, shear_range= 0.2, zoom_range= 0.2, horizontal_flip= True, vertical_flip= True)
+    ts_gen = ImageDataGenerator()
+
+    train_gen = tr_gen.flow_from_dataframe( train_df, x_col= 'filepaths', y_col= 'labels', target_size= img_size, class_mode= 'categorical',
+                                        color_mode= 'rgb', shuffle= True, batch_size= batch_size)
+
+    valid_gen = ts_gen.flow_from_dataframe( valid_df, x_col= 'filepaths', y_col= 'labels', target_size= img_size, class_mode= 'categorical',
+                                            color_mode= 'rgb', shuffle= True, batch_size= batch_size)
+
+    test_gen = ts_gen.flow_from_dataframe( test_df, x_col= 'filepaths', y_col= 'labels', target_size= img_size, class_mode= 'categorical',
+                                        color_mode= 'rgb', shuffle= False, batch_size= batch_size)
+    
+    return train_gen, valid_gen, test_gen
