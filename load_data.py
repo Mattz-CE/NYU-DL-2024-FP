abd6072f62c0891a685c82070d0a31ac7b007cc1
Matthew Zhou <Mattz0516@outlook.com>
2024-05-04 05:26:40 -0400

    TORCH SUPPORT

--- load_data.py
+++ load_data.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report
 
-# import Deep learning Libraries
+# import Tensorflowww Libraries
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
@@ -19,6 +19,13 @@ from tensorflow.keras.optimizers import Adam, Adamax
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Activation, Dropout, BatchNormalization
 from tensorflow.keras import regularizers
+
+# import Pytorch Libraries
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from PIL import Image
 
 # Ignore Warnings
 import warnings
@@ -28,7 +35,10 @@ print ('modules loaded')
 
 # unzip the data
 if not os.path.exists('lung_colon_image_set'):
-    !unzip archive.zip
+    # unzip archive.zip
+    import zipfile
+    with zipfile.ZipFile('archive.zip', 'r') as zip_ref:
+        zip_ref.extractall('.')
     
 # Generate data paths with labels
 data_dir = 'lung_colon_image_set'
@@ -75,6 +85,7 @@ train_df, dummy_df = train_test_split(df,  train_size= 0.8, shuffle= True, rando
 strat = dummy_df['labels']
 valid_df, test_df = train_test_split(dummy_df,  train_size= 0.5, shuffle= True, random_state= 123, stratify= strat)
 
+
 def view_image(n, df=df):
     img = Image.open(df['filepaths'].iloc[n])
     plt.imshow(img)
@@ -83,59 +94,6 @@ def view_image(n, df=df):
     plt.show()
     
 def load_data(df, batch_size):
-    # unzip the data
-    if not os.path.exists('lung_colon_image_set'):
-        # unzip archive.zip
-        import zipfile
-        with zipfile.ZipFile('archive.zip', 'r') as zip_ref:
-            zip_ref.extractall('.')
-        
-        
-    # Generate data paths with labels
-    data_dir = 'lung_colon_image_set'
-    filepaths = []
-    labels = []
-
-    folds = os.listdir(data_dir)
-    for fold in folds:
-        foldpath = os.path.join(data_dir, fold)
-        flist = os.listdir(foldpath)
-
-        for f in flist:
-            f_path = os.path.join(foldpath, f)
-            filelist = os.listdir(f_path)
-
-            for file in filelist:
-                fpath = os.path.join(f_path, file)
-                filepaths.append(fpath)
-
-                if f == 'colon_aca':
-                    labels.append('Colon Adenocarcinoma')
-
-                elif f == 'colon_n':
-                    labels.append('Colon Benign Tissue')
-
-                elif f == 'lung_aca':
-                    labels.append('Lung Adenocarcinoma')
-
-                elif f == 'lung_n':
-                    labels.append('Lung Benign Tissue')
-
-                elif f == 'lung_scc':
-                    labels.append('Lung Squamous Cell Carcinoma')
-
-    # Concatenate data paths with labels into one dataframe
-    Fseries = pd.Series(filepaths, name= 'filepaths')
-    Lseries = pd.Series(labels, name='labels')
-    df = pd.concat([Fseries, Lseries], axis= 1)
-    
-    strat = df['labels']
-    train_df, dummy_df = train_test_split(df,  train_size= 0.8, shuffle= True, random_state= 123, stratify= strat)
-
-    # valid and test dataframe
-    strat = dummy_df['labels']
-    valid_df, test_df = train_test_split(dummy_df,  train_size= 0.5, shuffle= True, random_state= 123, stratify= strat)
-    
     # crobed image size
     batch_size = 32
     img_size = (224, 224)
@@ -156,3 +114,48 @@ def load_data(df, batch_size):
                                         color_mode= 'rgb', shuffle= False, batch_size= batch_size)
     
     return train_gen, valid_gen, test_gen
+
+def load_data_for_pytorch(df, batch_size):
+    # Define transformations
+    transform = transforms.Compose([
+        transforms.Resize(224),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+    
+    # Creating a custom dataset
+    class ImageDataset(torch.utils.data.Dataset):
+        def __init__(self, dataframe, transform=None):
+            self.dataframe = dataframe
+            self.transform = transform
+
+        def __len__(self):
+            return len(self.dataframe)
+
+        def __getitem__(self, idx):
+            img_name = self.dataframe.iloc[idx, 0]
+            image = Image.open(img_name).convert('RGB')
+            label = self.dataframe.iloc[idx, 1]
+            
+            if self.transform:
+                image = self.transform(image)
+            
+            return image, label
+
+    # Split the dataframe into train, valid, test
+    strat = df['labels']
+    train_df, dummy_df = train_test_split(df, train_size=0.8, shuffle=True, random_state=123, stratify=strat)
+    valid_df, test_df = train_test_split(dummy_df, train_size=0.5, shuffle=True, random_state=123, stratify=dummy_df['labels'])
+
+    # Instantiate datasets
+    train_dataset = ImageDataset(train_df, transform=transform)
+    valid_dataset = ImageDataset(valid_df, transform=transform)
+    test_dataset = ImageDataset(test_df, transform=transform)
+
+    # Create data loaders
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    
+    return train_loader, valid_loader, test_loader
